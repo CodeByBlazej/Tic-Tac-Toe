8660d315fcba2ec6219d668d2ebfa0ba90089922
CodeByBlazej <codebyblazej@gmail.com>
2024-08-21 18:38:20 +0100

    test board object

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,17 @@
 require_relative 'lib/players'
+require_relative 'lib/board'
 
 adam = Player.new('Adam', 'X')
 puts adam.info
 
 blazej = Player.new('Blazej', 'O')
 puts blazej.info
+
+new_board = Board.new
+p new_board.board[0]
+p new_board.board[1]
+p new_board.board[2]
+
+new_board.board[0][0] = 'O'
+p new_board.board[0]
+
