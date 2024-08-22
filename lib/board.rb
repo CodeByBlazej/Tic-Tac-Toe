63a38e3d872902bce05ab3f8d8400c875b033e26
CodeByBlazej <codebyblazej@gmail.com>
2024-08-22 19:05:51 +0100

    add method to check score not finished

--- lib/board.rb
+++ lib/board.rb
@@ -5,6 +5,14 @@ class Board
     @board = Array.new(3) { Array.new(3) }
   end
 
+  def check_score
+    @board.each_with_index do |row, row_index|
+      row.each_with_index do |symbol, column_index|
+        p row[1]
+      end
+    end
+  end
+
   def display_board
     p @board[0]
     p @board[1]
