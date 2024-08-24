f1604a9fb949b8b7793bf4ff99991186abe31a4a
CodeByBlazej <codebyblazej@gmail.com>
2024-08-24 08:40:35 +0100

    get name and symbol input from player

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -21,19 +21,28 @@ new_board.display_board
 
 
 def play_game
-  kamil = Player.new('Kamil', 'O')
-  tomek = Player.new('Tomek', 'X')
+  player_1_name = gets.chomp
+  player_1_symbol = gets.chomp
+
+  player_2_name = gets.chomp
+  player_2_symbol = gets.chomp
+
+  player_1 = Player.new(player_1_name, player_1_symbol)
+  player_2 = Player.new(player_2_name, player_2_symbol)
+
   main_board = Board.new
   main_board.display_board
   # player_choice = gets.chomp
   p main_board.check_row_score
-  main_board.board[0][0] = kamil.symbol
-  main_board.board[0][1] = kamil.symbol
-  main_board.board[0][2] = tomek.symbol
+  main_board.board[0][0] = player_1.symbol
+  main_board.board[0][1] = player_1.symbol
+  main_board.board[0][2] = player_2.symbol
 
   p main_board.check_row_score
   main_board.display_board
   p main_board.check_row_score
+  p player_1.symbol
+  p player_1.name
 end
 
 play_game
