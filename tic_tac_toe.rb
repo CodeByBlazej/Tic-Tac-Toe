1aff5bd27d7303d4cc2312fffb689b000735dd1b
CodeByBlazej <codebyblazej@gmail.com>
2024-08-25 13:35:16 +0100

    check diagonal and antidiagonal methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,6 @@
-require_relative 'lib/players'
-require_relative 'lib/board'
+require_relative 'lib/spec/players'
+require_relative 'lib/spec/board'
+
 
 adam = Player.new('Adam', 'X')
 puts adam.info
@@ -20,29 +21,36 @@ new_board.display_board
 
 
 
-def play_game
-  player_1_name = gets.chomp
-  player_1_symbol = gets.chomp
+player_1_name = gets.chomp
+player_1_symbol = gets.chomp
 
-  player_2_name = gets.chomp
-  player_2_symbol = gets.chomp
+player_2_name = gets.chomp
+player_2_symbol = gets.chomp
+player_1 = Player.new(player_1_name, player_1_symbol)
+player_2 = Player.new(player_2_name, player_2_symbol)
+# def play_game
 
-  player_1 = Player.new(player_1_name, player_1_symbol)
-  player_2 = Player.new(player_2_name, player_2_symbol)
 
   main_board = Board.new
   main_board.display_board
   # player_choice = gets.chomp
-  p main_board.check_row_score
-  main_board.board[0][0] = player_1.symbol
-  main_board.board[0][1] = player_1.symbol
-  main_board.board[0][2] = player_2.symbol
-
-  p main_board.check_row_score
+  # p check_row_score
+  main_board.board[0][2] = player_1.symbol
+  main_board.board[1][1] = player_1.symbol
+  main_board.board[2][0] = player_1.symbol
+  # def check_row_score
+  # end
+  # p check_row_score
   main_board.display_board
-  p main_board.check_row_score
+  # p check_row_score
   p player_1.symbol
   p player_1.name
-end
+  # end
+  p main_board.board[0].all?(player_1.symbol)
+  p main_board.check_row_score(player_1)
+  p main_board.check_column_score(player_1)
+  p main_board.check_diagonal_score(player_1)
+  p main_board.check_anti_diagonal_score(player_1)
+
 
 play_game
