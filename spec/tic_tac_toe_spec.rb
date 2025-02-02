935d279f653021bcdc909a8b1084eaf9214ef742
CodeByBlazej <codebyblazej@gmail.com>
2025-02-02 20:21:52 +0000

    add more tests to test 2 first board methods

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -4,25 +4,85 @@ require_relative '../lib/sort/players'
 
 describe Board do
   describe '#check_row_score' do
-    subject(:board) { described_class.new(Array.new(3) { Array.new(3, 'X') }) }
-    let(:player) { instance_double(Player, name: 'Tom', symbol: 'X') }
-
     context 'when row scores are all the same' do
+      subject(:board) { described_class.new(Array.new(3) { Array.new(3, 'X') }) }
+      let(:player) { instance_double(Player, name: 'Tom', symbol: 'X') }
+
       before do
-        string = '#{player.name} WON THE GAME!'
         allow(board).to receive(:puts)
       end
 
       it 'puts player name WON THE GAME!' do
-        # result = board.check_row_score(player)
-        expect(board).to receive(:puts).with('Tom WON THE GAME!').once
+        expect(board).to receive(:puts).with("#{player.name} WON THE GAME!").once
         board.check_row_score(player)
       end
 
-      xit 'set winner to be true' do
-        # player = 'Tom'
+      it 'set winner to be true' do
         result = board.check_row_score(player)
         expect(result).to be_truthy
+        expect(board.winner).to eq(true)
+      end
+    end
+
+    context 'when row scores are not all the same' do
+      subject(:board) { described_class.new(Array.new(3) { Array.new(3) }) }
+      let(:player) { instance_double(Player, name: 'Alex', symbol: 'O') }
+
+      before do
+        allow(board).to receive(:puts)
+      end
+
+      it 'does not puts player name WON THE GAME!' do
+        expect(board).not_to receive(:puts).with("#{player.name} WON THE GAME!")
+        board.check_row_score(player)
+      end
+
+      it 'does not set winner to be true' do
+        result = board.check_row_score(player)
+        expect(result).not_to be_truthy
+        expect(board.winner).to be(false)
+      end
+    end
+  end
+
+  describe '#check_column_score' do
+    context 'when column scores are all the same' do
+      subject(:board) { described_class.new(Array.new(3) { Array.new(['X', ' ', ' ']) }) }
+      let(:player) { instance_double(Player, name: 'Tom', symbol: 'X') }
+
+      before do
+        allow(board).to receive(:puts)
+      end
+
+      it 'puts player name WON THE GAME!' do
+        expect(board).to receive(:puts).with("#{player.name} WON THE GAME!")
+        board.check_column_score(player)
+      end
+
+      it 'set winner to be true' do
+        result = board.check_column_score(player)
+        expect(result).to be_truthy
+        expect(board.winner).to eq(true)
+      end
+    end
+
+    context 'when column scores are not all the same' do
+      subject(:board) { described_class.new(Array.new(3) { Array.new(3) }) }
+      let(:player) { instance_double(Player, name: 'Alex', symbol: 'O') }
+
+      before do
+        allow(board).to receive(:puts)
+      end
+
+      it 'does not puts player name WON THE GAME!' do
+        expect(board).not_to receive(:puts).with("#{player.name} WON THE GAME!")
+        board.check_column_score(player)
+      end
+
+      it 'does not set winner to be true' do
+        result = board.check_column_score(player)
+        expect { result }.not_to(change { board.winner })
+        expect(board.winner).to eq(false)
       end
     end
   end
