367aa69d25115dcd51b34eb9b87578ac93a48d4b
CodeByBlazej <codebyblazej@gmail.com>
2025-02-03 19:01:14 +0000

    finish writing tests for all methods in Board class

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -24,7 +24,7 @@ describe Board do
       end
     end
 
-    context 'when row scores are not all the same' do
+    context 'when row scores are NOT all the same' do
       subject(:board) { described_class.new(Array.new(3) { Array.new(3) }) }
       let(:player) { instance_double(Player, name: 'Alex', symbol: 'O') }
 
@@ -32,12 +32,12 @@ describe Board do
         allow(board).to receive(:puts)
       end
 
-      it 'does not puts player name WON THE GAME!' do
+      it 'does NOT puts player name WON THE GAME!' do
         expect(board).not_to receive(:puts).with("#{player.name} WON THE GAME!")
         board.check_row_score(player)
       end
 
-      it 'does not set winner to be true' do
+      it 'does NOT set winner to be true' do
         result = board.check_row_score(player)
         expect(result).not_to be_truthy
         expect(board.winner).to be(false)
@@ -66,7 +66,7 @@ describe Board do
       end
     end
 
-    context 'when column scores are not all the same' do
+    context 'when column scores are NOT all the same' do
       subject(:board) { described_class.new(Array.new(3) { Array.new(3) }) }
       let(:player) { instance_double(Player, name: 'Alex', symbol: 'O') }
 
@@ -74,15 +74,111 @@ describe Board do
         allow(board).to receive(:puts)
       end
 
-      it 'does not puts player name WON THE GAME!' do
+      it 'does NOT puts player name WON THE GAME!' do
         expect(board).not_to receive(:puts).with("#{player.name} WON THE GAME!")
         board.check_column_score(player)
       end
 
-      it 'does not set winner to be true' do
+      it 'does NOT set winner to be true' do
         result = board.check_column_score(player)
         expect { result }.not_to(change { board.winner })
         expect(board.winner).to eq(false)
+      end
+    end
+  end
+
+  describe '#check_diagonal_score' do
+    context 'when diagonal scores are all the same' do
+      subject(:board) { described_class.new(Array.new(3) { Array.new(3) }) }
+      let(:player) { instance_double(Player, name: 'Tom', symbol: 'X') }
+
+      before do
+        board.board[0][0] = 'X'
+        board.board[1][1] = 'X'
+        board.board[2][2] = 'X'
+        allow(board).to receive(:puts)
+      end
+
+      it 'puts player name WON THE GAME!' do
+        expect(board).to receive(:puts).with("#{player.name} WON THE GAME!")
+        board.check_diagonal_score(player)
+      end
+
+      it 'set winner to be true' do
+        result = board.check_diagonal_score(player)
+        expect(result).to be_truthy
+        expect(board.winner).to be(true)
+      end
+    end
+
+    context 'when diagonal scores are NOT all the same' do
+      subject(:board) { described_class.new(Array.new(3) { Array.new(3) }) }
+      let(:player) { instance_double(Player, name: 'Blazej', symbol: 'O') }
+
+      before do
+        board.board[0][0] = 'O'
+        board.board[1][1] = 'X'
+        board.board[2][2] = 'O'
+        allow(board).to receive(:puts)
+      end
+
+      it 'does NOT puts player name WON THE GAME!' do
+        expect(board).not_to receive(:puts).with("#{player.name} WON THE GAME!")
+        board.check_diagonal_score(player)
+      end
+
+      it 'does NOT set winner to be true' do
+        result = board.check_diagonal_score(player)
+        expect(result).not_to be_truthy
+        expect(board.winner).to be(false)
+      end
+    end
+  end
+
+  describe '#check_anti_diagonal_score' do
+    context 'when anti diagonal scores are all the same' do
+      subject(:board) { described_class.new(Array.new(3) { Array.new(3) }) }
+      let(:player) { instance_double(Player, name: 'Tomassi', symbol: 'X') }
+
+      before do
+        board.board[0][2] = 'X'
+        board.board[1][1] = 'X'
+        board.board[2][0] = 'X'
+        allow(board).to receive(:puts)
+      end
+
+      it 'puts player name WON THE GAME!' do
+        expect(board).to receive(:puts).with("#{player.name} WON THE GAME!")
+        board.check_anti_diagonal_score(player)
+      end
+
+      it 'set winner to be true' do
+        result = board.check_anti_diagonal_score(player)
+        expect(result).to be_truthy
+        expect(board.winner).to be(true)
+      end
+    end
+
+    context 'when anti diagonal scores are NOT all the same' do
+      subject(:board) { described_class.new(Array.new(3) { Array.new(3) }) }
+      let(:player) { instance_double(Player, name: 'Alexis', symbol: 'O') }
+
+      before do
+        board.board[0][2] = 'O'
+        board.board[1][1] = 'X'
+        board.board[2][0] = 'O'
+        allow(board).to receive(:puts)
+      end
+
+      it 'does NOT puts player name WON THE GAME!' do
+        expect(board).not_to receive(:puts).with("#{player.name} WON THE GAME!")
+        board.check_anti_diagonal_score(player)
+      end
+
+      it 'does NOT set winner to be true' do
+        result = board.check_anti_diagonal_score(player)
+        expect(result).not_to be_truthy
+        expect(board.winner).to be(false)
       end
     end
   end
