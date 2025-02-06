1b80b5972ae3934b15b21af95b3f5e1be92658bf
CodeByBlazej <codebyblazej@gmail.com>
2025-02-06 19:27:58 +0000

    add tests to play_round method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -78,18 +78,60 @@ describe Game do
   end
 
   describe '#play_round' do
-    context 'after player1 chose available row and column' do
+    context 'when the chosen spot is empty' do
       subject(:game) { described_class.new }
       let(:player1) { instance_double(Player, name: 'Tom', symbol: 'X') }
+      let(:board_array) { Array.new(3) { Array.new(3) } }
       let(:main_board) { instance_double(Board) }
 
       before do
         game.instance_variable_set(:@player1, player1)
         game.instance_variable_set(:@main_board, main_board)
+
+        allow(game).to receive(:gets).and_return('0', '0')
+        allow(game).to receive(:puts)
+        allow(main_board).to receive(:board).and_return(board_array)
+        allow(main_board).to receive(:display_board)
+        allow(main_board).to receive(:check_row_score).with(player1)
+        allow(main_board).to receive(:check_column_score).with(player1)
+        allow(main_board).to receive(:check_diagonal_score).with(player1)
+        allow(main_board).to receive(:check_anti_diagonal_score).with(player1)
       end
 
       it 'checks for every score on the board' do
-        expect
+        expect(main_board).to receive(:check_row_score).with(player1)
+        expect(main_board).to receive(:check_column_score).with(player1)
+        expect(main_board).to receive(:check_diagonal_score).with(player1)
+        expect(main_board).to receive(:check_anti_diagonal_score).with(player1)
+        game.play_round(player1, main_board)
+        expect(board_array[0][0]).to eq('X')
+      end
+    end
+
+    context 'when the chosen spot is already taken' do
+      subject(:game) { described_class.new }
+      let(:player1) { instance_double(Player, name: 'Frank', symbol: 'O') }
+      let(:board_array) { Array.new(3) { Array.new(3) } }
+      let(:main_board) { instance_double(Board) }
+
+      before do
+        board_array[1][1] = 'X'
+
+        game.instance_variable_set(:@main_board, main_board)
+        allow(game).to receive(:gets).and_return('1', '1', '0', '0')
+        allow(game).to receive(:puts)
+        allow(main_board).to receive(:board).and_return(board_array)
+        allow(main_board).to receive(:display_board)
+        allow(main_board).to receive(:check_row_score).with(player1)
+        allow(main_board).to receive(:check_column_score).with(player1)
+        allow(main_board).to receive(:check_diagonal_score).with(player1)
+        allow(main_board).to receive(:check_anti_diagonal_score).with(player1)
+      end
+
+      it 'puts this spot is already taken!' do
+        expect(game).to receive(:puts).with('This spot is already taken, please choose another one.').once
+        game.play_round(player1, main_board)
+        expect(board_array[0][0]).to eq('O')
       end
     end
   end
